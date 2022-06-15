fb300aea6cd7e1b17f9ac2bd69f1cf221a2b07f8
Rick Kayo <rkayo@elite-em.local>
2022-06-14 21:14:11 -0700

    Complete task 3

--- index.js
+++ index.js
@@ -44,9 +44,8 @@ Use getFinals to do the following:
 */
 
 function getFinals(arr) {
-    /* code here */
+    return arr.filter(arr => arr.Stage === "Final");
  }
-
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
@@ -55,10 +54,11 @@ Use the higher-order function called getYears to do the following:
 2. Receive a callback function as the second parameter that will take getFinals from task 2 as an argument
 3. Return an array called years containing all of the years in the getFinals data set*/
 
-function getYears(/* code here */) {
-    /* code here */
+function getYears(arr,cb) {
+    let years = cb(arr).map(arr => arr['Year']);
+    return years;
 }
-
+console.log(getYears(fifaData,getFinals));
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
